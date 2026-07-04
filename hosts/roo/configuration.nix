d90ee1d50b9c5a1c7bf531db84f54c8909765f97
Mark Cyster <mcyster@gmail.com>
2026-07-03 19:46:00 -0700

    roo: add plants app

--- hosts/roo/configuration.nix
+++ hosts/roo/configuration.nix
@@ -24,11 +24,21 @@
   services.httpd = {
     enable = true;
     adminAddr = "webmaster@localhost";
+    extraModules = [ "proxy" "proxy_http" ];
     virtualHosts."cyster.com" = {
       documentRoot = "/srv/www/cyster.com/public";
       enableACME = true;
       forceSSL = true;
       serverAliases = [ "www.cyster.com" ];
+    };
+    virtualHosts."plants.cyster.com" = {
+      enableACME = true;
+      forceSSL = true;
+      extraConfig = ''
+        ProxyPreserveHost On
+        ProxyPass / http://127.0.0.1:8001/
+        ProxyPassReverse / http://127.0.0.1:8001/
+      '';
     };
   };
 
@@ -40,7 +50,26 @@
     "d /srv/www 0755 root root - -"
     "d /srv/www/cyster.com 0755 wal users - -"
     "d /srv/www/cyster.com/public 0755 wal users - -"
+    "d /srv/game1 0755 wal users - -"
+    "d /srv/game1/instance 0755 wal users - -"
   ];
+
+  systemd.services.game1 = {
+    description = "Plant Collector Flask app";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    unitConfig.ConditionPathExists = "/srv/game1/scripts/wsgi.py";
+    serviceConfig = {
+      User = "wal";
+      Group = "users";
+      WorkingDirectory = "/srv/game1";
+      EnvironmentFile = "/srv/game1/instance/game1.env";
+      ExecStart = "${pkgs.nix}/bin/nix develop --command gunicorn --bind 127.0.0.1:8001 --workers 2 scripts.wsgi:app";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
 
   security.sudo.wheelNeedsPassword = false;
 
