75013d3d3c0d2bcf6c2d6824d3cc7796d33eb36e
Mark Cyster <mcyster@gmail.com>
2026-07-04 10:43:25 -0700

    base: restore packages and nh config from removed my.nix

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -2,9 +2,29 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  programs.nh = {
+    enable = true;
+    flake = "/home/wal/nixos-config";
+  };
+
   environment.systemPackages = with pkgs; [
     git
     nh
     vim
+    # Restored from old modules/my.nix
+    direnv
+    opencode
+    ripgrep
+    btop
+    jq
+    wget
+    zip
+    unzip
+    usbutils
+    pciutils
+    lshw
+    lsof
+    acpi
+    dmidecode
   ];
 }
